c7cb9bf6d662b41d5d6ccb6d3388e7d762452532
aa <170978221+DeveloperNoxis@users.noreply.github.com>
2026-08-26 21:52:52 -0300

    feat(player): abstract class

---
+++ Fantasy/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class Player : MonoBehaviour
+{
+    public abstract float movSpeed { get; }
+    public abstract float runSpeed { get; }
+    public abstract float crouchSpeed { get; }
+
+
+    protected float speedX, speedY;
+    protected Rigidbody2D rb;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+       
+    }
+
+    
+    void Move()
+    {
+    
+    }
+}
--- Fantasy/Assets/Scripts/Player/PlayerControll.cs
+++ Fantasy/Assets/Scripts/Player/PlayerControll.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class PlayerControll : MonoBehaviour
+public class PlayerControll : Player
 {
-    public float movSpeed;
-    float speedX, speedY;
-    Rigidbody2D rb;
+    public override float movSpeed => 5f;
+    public override float runSpeed => 8f;
+    public override float crouchSpeed => 2.5f;
+
+    public Rigidbody2D rsb;
 
     void Start()
     {
@@ -15,8 +17,28 @@ public class PlayerControll : MonoBehaviour
 
     void Update()
     {
-        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
-        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        Move();
+    }
+
+    void Move()
+    {
+        // 1. Descobre a velocidade atual com base nas teclas pressionadas
+        float currentSpeed = movSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = runSpeed;
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            currentSpeed = crouchSpeed;
+        }
+
+        // 2. Aplica a velocidade nos eixos de movimento
+        speedX = Input.GetAxisRaw("Horizontal") * currentSpeed;
+    
+
+        // 3. Move o Rigidbody2D
         rb.linearVelocity = new Vector2(speedX, speedY);
     }
 }
